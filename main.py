9d5f8783f6f4366bef6b81606051b4cb1225e19e
BluABK <bluabk@bluabk.net>
2021-09-08 17:37:58 +0200

    Split util funcs into utils.py, implemented deletion api and refactored common code between it and history api.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import json
+import time
+
 import requests
-from requests import Request, Response
 
 from handlers.config_handler import load_config
 from handlers.log_handler import create_logger
 from puush_entry import PuushEntry
+from utils import logprint_request, logprint_response, logprint
 
 log = create_logger(__name__)
 
@@ -18,44 +19,17 @@ UPLOAD_API = "{}/up".format(API)
 config = load_config()
 
 
-def log_request(url: str, method="POST"):
-    log.info("Sent {method} request to {url}".format(method=method, url=url))
+def make_post_request(api_endpoint: str, data: dict):
+    """
+    Makes a HTTP POST request to an API endpoint, with an optional data payload.
+    :param api_endpoint:
+    :param data:
+    :return:
+    """
+    response = requests.post(api_endpoint, data=data)
+    logprint_request(api_endpoint)
 
-
-def print_request(url: str, method="POST"):
-    print("Sent {method} request to {url}".format(method=method, url=url))
-
-
-def log_response(response: Response):
-    log.info("Received {method} request response {code} ({reason}){server} "
-             "in {elapsed}".format(
-                method=response.request.method.upper(),
-                server=" from server '{}'".format(
-                    response.headers.get("Server")) if "Server" in response.headers.keys() else "",
-                elapsed=str(response.elapsed),
-                reason=response.reason,
-                code=response.status_code))
-
-
-def print_response(response: Response):
-    print("Received {method} request response {code} ({reason}){server} "
-          "in {elapsed}".format(
-                method=response.request.method.upper(),
-                server=" from server '{}'".format(
-                    response.headers.get("Server")) if "Server" in response.headers.keys() else "",
-                elapsed=str(response.elapsed),
-                reason=response.reason,
-                code=response.status_code))
-
-
-def get_history():
-    entries = []
-    response = requests.post(HISTORY_API, data={"k": config["api_key"]})
-    log_request(HISTORY_API)
-    print_request(HISTORY_API)
-
-    log_response(response)
-    print_response(response)
+    logprint_response(response)
     log.debug("Response dict:\n{}".format(response.__dict__))
 
     # Split response text into list of strings.
@@ -73,13 +47,68 @@ def get_history():
         if response_lines[i] == "":
             response_lines.pop(i)
 
-    for line in response_lines:
+    return response_lines
+
+
+def response_texts_to_entries(texts: list):
+    """
+    Takes a list of strings and creates a list of PuushEntry objects from it.
+    :param texts:
+    :return:
+    """
+    entries = []
+
+    # Remove empty text entries.
+    for i in range(len(texts)):
+        if texts[i] == "":
+            texts.pop(i)
+
+    for text in texts:
         # Split string into a list.
-        properties = line.split(',')
+        properties = text.split(',')
 
         entries.append(PuushEntry(*properties))
 
     return entries
+
+
+def get_history():
+    """
+    Puush History API request which returns up to 10 entries, if successful.
+    :return:
+    """
+    return response_texts_to_entries(make_post_request(HISTORY_API, data={"k": config["api_key"]}))
+
+
+def delete_puush_entry(entry: PuushEntry):
+    """
+    Puush Deletion API request which deletes a given PuushEntry and
+    returns an updated list of puush (history?) entries.
+    :param entry:
+    :return:
+    """
+    if entry.identifier is None:
+        raise Exception("PuushEntry identifier was None!")
+
+    logprint("Deleting Puush entry \"{name}\" (ID: {ident})...".format(name=entry.filename,
+                                                                       ident=entry.identifier))
+
+    # Delete the given puush by id and store the updated list of puush (history?) entries
+    return response_texts_to_entries(make_post_request(
+        DELETION_API, data={"k": config["api_key"], "i": entry.identifier}))
+
+
+def add_unique_puush_entries(src: list, dst: list):
+    """
+    Adds items from source list to destination list,
+    if they are not already in destination list.
+    :param src:
+    :param dst:
+    :return:
+    """
+    for src_entry in src:
+        if not any(dst_entry.identifier == src_entry.identifier for dst_entry in dst):
+            dst.append(src_entry)
 
 
 if __name__ == '__main__':
@@ -97,7 +126,26 @@ if __name__ == '__main__':
         log.info([str(x) for x in history_entries])
         for entry in history_entries:
             print(entry)
+
+        # While history holds item, systematically delete them.
+        for puush in history_entries:
+            try:
+                # Stagger deletions to not spam API.
+                time.sleep(0.5)
+
+                # Perform deletion and add new unique entries to the history.
+                add_unique_puush_entries(history_entries, delete_puush_entry(puush))
+                log.debug2(history_entries)
+
+                for entry in history_entries:
+                    log.debug(entry)
+            except Exception as exc:
+                log.exception(exc)
+                print("{}: {}".format(exc.__class__.__name__, str(exc)))
+                raise
+
     except Exception as exc:
         log.exception(exc)
         print("{}: {}".format(exc.__class__.__name__, str(exc)))
+        raise
 
---
+++ utils.py
@@ -0,0 +1,50 @@
+from requests import Response
+
+from handlers.log_handler import create_logger
+
+log = create_logger(__name__)
+
+
+def log_request(url: str, method="POST"):
+    log.info("Sent {method} request to {url}".format(method=method, url=url))
+
+
+def print_request(url: str, method="POST"):
+    print("Sent {method} request to {url}".format(method=method, url=url))
+
+
+def logprint_request(url: str, method="POST"):
+    log_request(url, method)
+    print_request(url, method)
+
+
+def log_response(response: Response):
+    log.info("Received {method} request response {code} ({reason}){server} "
+             "in {elapsed}".format(
+                method=response.request.method.upper(),
+                server=" from server '{}'".format(
+                    response.headers.get("Server")) if "Server" in response.headers.keys() else "",
+                elapsed=str(response.elapsed),
+                reason=response.reason,
+                code=response.status_code))
+
+
+def print_response(response: Response):
+    print("Received {method} request response {code} ({reason}){server} "
+          "in {elapsed}".format(
+                method=response.request.method.upper(),
+                server=" from server '{}'".format(
+                    response.headers.get("Server")) if "Server" in response.headers.keys() else "",
+                elapsed=str(response.elapsed),
+                reason=response.reason,
+                code=response.status_code))
+
+
+def logprint_response(response: Response):
+    log_response(response)
+    print_response(response)
+
+
+def logprint(anything: any):
+    log.info(anything)
+    print(anything)
